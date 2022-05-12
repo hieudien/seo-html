fc2770bdd3179cbfe18ae1aed6ac03edb0b281e6
Hieuu Doan <hieu@wiserbot.com>
2022-05-12 11:35:34 +0700

    SEO HTML init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import "quill/dist/quill.snow.css";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
